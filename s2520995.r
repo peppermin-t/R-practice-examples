fd8585ee5f85052353efd9931da3332363d07086
peppermin-t <chenyinjia2000@gmail.com>
2023-11-03 05:11:56 +0000

    comments done

--- s2520995.r
+++ s2520995.r
@@ -64,7 +64,7 @@ eta_vec <- function(x, xk) {
 # Parameter:
 #   x (size: n, 2): the input x of the data to be fitted, containing n
 #     2-dimensional points
-#   k (size: scalar): number of the control points
+#   k (size: scalar): number of the control points (default is 100)
 getTPS <- function(x, k=100) {
 
   # ensure k is not less than or equal to 3, otherwise stop the programme,
@@ -130,93 +130,196 @@ getTPS <- function(x, k=100) {
   # assign the top left k-3, k-3 submatrix with (Z^T)Ek(Z) to get final S
   S[1 : (k - 3), 1 : (k - 3)] <- t(Z) %*% Ek %*% Z  # S: [k, k]
 
-  # return the needed objects as a list, including xk, X, S, and qrtk
+  # return the needed objects as a list, including xk, X, S, and Z-related
+  # infromation qrtk
   list(xk=xk, X=X, S=S, qrtk=qrtk)
 }
 
-getV <- function(tps, y, lambda, evrsr, R, qrx) {
+# Define function "getV"
+# Aims to calculate the genralized cross validation (GCV) value, the minimizing
+# objective, for a single smoothing parameter, lambda, trial with needed
+# matrices provided.
+# Parameter:
+#   X (size: n, k): the X matrix generated during getTPS in fitting
+#   y (size: n,): the output y of the data to be fitted, containing n
+#     single values
+#   lambda (size: scalar): the lambda value of this GCV calculation trial
+#   R (size: k, k): the R matrix of the QR decomposition result of X
+#   evrsr: the result information of the eigen-decomposition for (R^-T)S(R^-1)
+#   qrx: the result information of the QR-decomposition of X
+getV <- function(X, y, lambda, R, evrsr, qrx) {
+  # obtain the up-of-date k and n values from length of y and row numbers of R
   n <- length(y)
   k <- nrow(R)
 
-  X <- tps$X
+  # retrieve the corresponding eigen vector matrix, U,
+  # and the eigen values, ev, from evrsr
+  U <- evrsr$vectors  # k, k
+  ev <- evrsr$values  # k,
 
-  U <- evrsr$vectors
-  ev <- evrsr$values
+  # generate the diagonal matrix Diag with 1 + lambda * ev_i using diag(),
+  # i.e. I + (lambda)*(Lambda)
+  Diag <- diag(1 + lambda * ev)  # k, k
 
-  Diag <- diag(1 + lambda * ev)
+  # generate the parameter beta under this lambda, by solving a linear
+  # transformation using solve(), with beta unknown. the rhs is obtained by
+  # calculating (Q^T)y (Q is the Q matrix of QR decomposition of X) implicitly
+  # with qr.qty to improve efficiency, and the lhs computes matrix
+  # multiplications, regarding U is an orthogonal matrix.
+  betah <- solve(U %*% Diag %*% t(U) %*% R, qr.qty(qrx, y)[1: k])  # k,
+  # note that qr.qty() use the transposed complete Q matrix to calculate
+  # the dot product with y, so subtracting the first k items of the result
+  # will be the expected k, size result vector
 
-  # eigenvalues. concise  # notice the "[1: k]"!
-  betah <- solve(U %*% Diag %*% t(U) %*% R, qr.qty(qrx, y)[1: k])
+  # calculate the effective degrees of freedom (EDF) of the model under the
+  # given lambda, by directly summing up the inverse of each elements of Diag's
+  # diagonal elements.
+  EDF <- sum(1 / diag(Diag))  # scalar
 
-  EDF <- sum(1 / diag(Diag))
+  # calculate the model prediction of y's expectation, mu, by multiplication
+  # between X and beta
+  muh <- X %*% betah  # n,
 
-  muh <- X %*% betah
+  # calculate the generalized cross validation (GCV) value, V, under the given
+  # lambda
+  V <- sum((y - muh) ** 2) / (n - EDF) ** 2  # scalar
 
-  list(V=sum((y - muh) ** 2) / (n - EDF) ** 2, beta=betah, mu=muh, EDF=EDF)
+  # return the GCV value and other values needed in this lambda trial as a list,
+  # including V, beta, mu, and EDF
+  list(V=V, beta=betah, mu=muh, EDF=EDF)
 }
 
+# Define function "fitTPS"
+# Aims to fit the spline to best minimizing the objective function and get the
+# best set of parameters, i.e. beta, of the spline, according to the given x and
+# y data, through finding the smoothing parameter, lambda, best minimizing the
+# generalized cross validation (GCV) value.
+# Parameter:
+#   x (size: n, 2): the input x of the data to be fitted, containing n
+#     2-dimensional points
+#   y (size: n,): the output y of the data to be fitted, containing n
+#     single values
+#   k (size: scalar): number of the control points (default is 100)
+#   lsp (size: 2,): the log scale range on which the smoothing parameter,
+#     lambda's search is spaced on, with lsp[1] being the lower bound, lsp[2]
+#     being the upper bound (default is c(-5, 5))
 fitTPS <- function(x, y, k=100, lsp=c(-5, 5)) {
 
-  # set up the TPS and retrieve return values
+  # first call getTPS to set up the matrices for the spline and prepare
+  # other variables needed for fitting, including X, S, and xk
   tps <- getTPS(x, k)
-  k <- nrow(tps$xk) # renew k
-  X <- tps$X
-  S <- tps$S
 
-  qrx <- qr(X)  # qr of X
+  # obtain the up-of-date k value from length of xk of tps
+  k <- nrow(tps$xk)
+  # retrieve the X and S matrix from the returned tps list
+  X <- tps$X  # n, k
+  S <- tps$S  # k, k
 
+  # do QR decomposition for X with qr(), save the result in qrx
+  qrx <- qr(X)
+
+  # retrieve the R matrix of X's QR decomposition with qr.R
   R <- qr.R(qrx)  # k, k
 
+  # with R being a upper triangular matrix, use backsolve to calculate
+  # its inverse, faster than normal solve
   Rinv <- backsolve(R, diag(k))  # k, k
 
-  RSR <- t(Rinv) %*% S %*% Rinv
+  # calculate the following term, RSR
+  RSR <- t(Rinv) %*% S %*% Rinv  # k, k
 
-  # checking symmetricy
-  evrsr <- eigen(RSR, symmetric=TRUE)  # more concise?
+  # do eigen-decomposition on RSR with eigen(), save the result in evrsr
+  # note that "symmetric=TRUE" is needed because the machine error can make
+  # RSR asymmetric by having tiny errors in the symmetric positions in the
+  # matrix (which suppose to be symmetric)
+  evrsr <- eigen(RSR, symmetric=TRUE)
 
+  # record the best Vpack (the list returned by getV, representing the results
+  # bundle in a single lambda trial), and the best lambda, initialize them with
+  # valid initial values (valid for later comparisons)
   vpackb <- list(V=Inf)
   lambdab <- 0
+
+  # initialize an empty vector to record the EDF value of each trial
   edfs <- vector()
 
+  # looping over the possible lambda values, which is 100 values between
+  # exp(lsp[1]) and exp(lsp[2])
   for (i in seq(lsp[1], lsp[2], length.out=100)) {
+    # get the lambda value this trial
     lambda <- exp(i)
-    vpack <- getV(tps, y, lambda, evrsr, R, qrx)
+    # calculate the results bundle of this trial, save it in vpack
+    vpack <- getV(X, y, lambda, R, evrsr, qrx)
+    # append the edf value this trial to the record, edfs
     edfs <- c(edfs, vpack$EDF)
 
+    # if the GCV value this trial is less than the current smallest GCV value,
+    # then renew the best lambda and the best results bundle
     if (vpack$V < vpackb$V) {
       lambdab <- lambda
       vpackb <- vpack
     }
   }
 
+  # ready to return the list tps_list, including best values of parameters beta,
+  # mu, EDF, lambda, and GCV, along with the edf vector, and other variables
+  # needed in plotting, including qrtk, xk, and x's largest and smallest
+  # coordinate values
   tps_list <- list(
     beta=vpackb$beta, mu=vpackb$mu, medf=vpackb$EDF,
     lambda=lambdab, gcv=vpackb$V, edf=edfs,
     qrtk=tps$qrtk, xk=tps$xk, x_range=c(min(x), max(x))
   )
+
+  # set the class of the return list as "tps"
   class(tps_list) <- "tps"
+
+  # return tps_list
   tps_list
 }
 
+# Define function "plot.tps"
+# Aims to plot the fitted thin plate spline with the best parameters
+# obtained from fitTPS
+# Parameter:
+#   tps: the list of objects of class "tps", returned by fitTPS, containing
+#     beta, mu, medf, lambda, gcv, edf, qrtk, xk, x_range, as mentioned above
 plot.tps <- function(tps) {
+  # obtain the up-of-date k value from length of beta of tps
   k <- length(tps$beta)
 
-  # obtain the parameters
+  # break beta vector into 2, with the first k-3 element being deltaz, and
+  # the last 3 elements being alpha (the weight params of the affine
+  # transformation)
   alpha <- tps$beta[(k - 2): k]  # 3
   deltaz <- tps$beta[1: (k - 3)]  # k - 3
+  # do multiplication beqween Z and deltaz to get the final parameters, delta
+  # (the weight params of the non-affine deformation).
+  # once again not using the explicit form of Z, instead using qr.qty, since
+  # Z is the last k-3 columns of the Q matrix from a QR decomposition of
+  # Tk_ to improve efficiency. In this case, deltaz should be extended 3
+  # locations of 0 in the front to get the desired result.
   delta <- qr.qy(tps$qrtk, c(rep(0, 3), deltaz))  # k
 
-  # visualize test ...
+  # creating testing grid of input x
+  # setting the side length of the testing grid, m, to 50
   m <- 50
-  x2 <- x1 <- seq(tps$x_range[1], tps$x_range[2], length=m)
-  xp <- cbind(rep(x1, m), rep(x2, each=m))
+  # generating the testing grid xp with the side length of m, lower bound of the
+  # samallest of x's coordinates, and upperbound of the largest one.
+  x2 <- x1 <- seq(tps$x_range[1], tps$x_range[2], length=m)  # m,
+  xp <- cbind(rep(x1, m), rep(x2, each=m))  # m * m, 2
 
-  # cbind is faster than rbind
+  # generate the corresponding prediction y results with vector products:
+  #   alpha and delta are mentioned above; the term multipied with alpha
+  #   is the model matrix of the affine transformation part, and the term
+  #   multipied with delta is the matrix similar to E, containing the structual
+  #   information between new xp and former xk
+  # note that using cbind is faster than rbind
   y <- cbind(matrix(1, m * m, 1), xp) %*% alpha +
-    t(apply(xp, 1, function(p) eta_vec(p, tps$xk))) %*% delta
+    t(apply(xp, 1, function(p) eta_vec(p, tps$xk))) %*% delta  # m * m,
 
+  # plot the contour of the generated spline according to the y and xp
   contour(x1, x2, matrix(y, m, m))
+  # adjust the perspective of the plot's demonstration
   persp(x1, x2, matrix(y, m, m), theta=30, phi=30)
 }
-
-# E's apply can be further simplified
